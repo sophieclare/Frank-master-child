f4f38128ea29c2c02caea6eda26a65c513f8d32b
Rich C <rich.cookson@gmail.com>
2014-03-21 15:04:51 +0000

    Tidied up the site map page layout

--- page-sitemap.php
+++ page-sitemap.php
@@ -7,24 +7,33 @@
 get_header();
 ?>
 
+<div id="content" class="page">
+  <div class="row">
 
-<div class="page">
-    <div class="column colspan3">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <main id="content-primary" class="column six" role="main">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="post" id="post-<?php the_ID(); ?>">
-			<h1><?php the_title(); ?></h1>
+
+				<h1><?php the_title(); ?></h1>
+
 				<div class="entry">
 					<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 					<div class="nestedColumnContainer sitemap">
 						<?php flexipages('show_subpages=1&sort_column=menu_order,name&hierarchy=1&child_of=0'); ?>
 					</div>
-
 				</div>
+
 			</div>
-		<?php endwhile; endif; ?>
-	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
-&nbsp;
+			<?php endwhile; endif; ?>
+			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+		</main>
+
+		<?php get_sidebar('page'); ?>
+
+
+
 	</div>
-<?php get_sidebar('page'); ?>
+
 </div>
 <?php get_footer(); ?>
